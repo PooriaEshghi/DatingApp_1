3537ca2d017abf7c788b9b01203e73f1dd02b691
Pooria Eshghi <pooriaeshghi@gmail.com>
2025-12-08 19:26:42 +0100

    Initial client app

---
+++ client/src/app/app.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient)
+  user: any;
+  protected readonly title = signal('DatingApp');
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+
+
+    }
+  }
+
+}
