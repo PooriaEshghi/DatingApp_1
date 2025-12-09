082f5edecc8163180e48318f4216ddf3ad51e093
Pooria Eshghi <pooriaeshghi@gmail.com>
2025-12-09 18:07:07 +0100

    Add Authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,10 +27,44 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    options =>
+   {
+       var tokenKey = builder.Configuration["TokenKey"]
+           ?? throw new Exception("Token key not found - Program.cs");
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+           ValidateIssuerSigningKey = true,
+           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+           ValidateIssuer = false,
+           ValidateAudience = false
+       };
+
+    //    options.Events = new JwtBearerEvents
+    //    {
+    //        OnMessageReceived = context =>
+    //        {
+    //            var accessToken = context.Request.Query["access_token"];
+
+    //            var path = context.HttpContext.Request.Path;
+    //            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+    //            {
+    //                context.Token = accessToken;
+    //            }
+
+    //            return Task.CompletedTask;
+    //        }
+    //    };
+   });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
